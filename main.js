53363d8a6ed21e54432366d670fc0bb1d593ffa8
mmarin16 <m.marin16@hotmail.com>
2014-06-02 23:31:03 -0500

    mlp
    ahi ta

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
  
-	$('form').submit(function(e) {
+	$('#login').submit(function(e) {
 		e.preventDefault();
  		
 		var data = $(this).serializeArray();
 		data.push({name: 'tag', value: 'login'});
 		//alert(data[2].value);
 		$.ajax({
-			url: 'buscar.php',
+			url: 'main.php',
 			type: 'POST',
 			dataType: 'json',
 			data: data
@@ -20,6 +20,47 @@ $(document).ready(function() {
 		})
 		.fail(function() {  //false
 			$('span').html("Datos incorrectos");
+		})
+		.always(function() {
+				
+		});
+ 		//alert('usuario si existe');
+		
+	})
+})
+
+$(document).ready(function() {
+ 
+	$('#forgot').submit(function(e) {
+		e.preventDefault();
+ 		
+		var data = $(this).serializeArray();
+		data.push({name: 'tag', value: 'forgot'});
+		//data.push({name:'email_text',value: document.getElementById('email').value});
+		//alert(data[0].value);
+		$.ajax({
+			url: 'main.php',
+			type: 'POST',
+			dataType: 'json',
+			data: data
+			
+		})
+		.done(function(resp) {  //true
+			//$('span').html("Codigo enviado a su email");
+			if (resp.error>0)
+			{
+				alert(resp.msg);
+			}
+			else
+			{
+				alert(resp.msg);
+				self.location="login.html";
+			}
+
+		})
+		.fail(function(resp) {  //false
+			alert(resp.error);
+			$('span').html("Error al procesar");
 		})
 		.always(function() {
 				
---
+++ main.php
@@ -0,0 +1,72 @@
+<?php
+
+//<script type="text/javascript">alert("Hola");</script>
+$tag = $_POST['tag'];
+
+if (isset($tag) && $tag !== '') {
+
+		if ($tag == 'login') 
+        {
+			//echo true;
+            $user =$_POST['username'];
+            $pass=$_POST['password'];
+			$conexion = mysql_connect("localhost", "root", "");
+    		mysql_select_db("prueba", $conexion);
+    		$queEmp = "SELECT * FROM usuarios where username='".$user."' and pass='".sha1($pass)."'";
+    		$resEmp = mysql_query($queEmp,$conexion)or die(mysql_error());
+		    if  (mysql_num_rows($resEmp)>0)
+    		{
+        		//<script language='JavaScript'>alert('usuario si existe'); </script>;
+        		echo true;
+    		}
+    		else
+    		{
+    			echo false;
+    		}
+			
+		}
+
+        elseif ($tag=='forgot')
+        {
+            include("class.phpmailer.php"); 
+            include("class.smtp.php"); 
+            $mail_destino=$_POST['email'];
+            $mail = new PHPMailer(); 
+            $mail->IsSMTP(); 
+            $mail->SMTPAuth = true; 
+            $mail->SMTPSecure = "ssl"; 
+            $mail->Host = "smtp.gmail.com"; 
+            $mail->Port = 465; 
+            $mail->Username = "mm.marin16@gmail.com"; 
+            $mail->Password = "alejandro1990";
+            $mail->From = "mm.marin16@gmail.com"; 
+            $mail->FromName = "Test Login"; 
+            $mail->Subject = "Recupera tu contraseña!"; 
+            $mail->AltBody = "Este es un mensaje de prueba."; 
+            $mail->MsgHTML("<b>Este es un mensaje de prueba</b>."); 
+            //$mail->AddAttachment("files/files.zip"; 
+            //$mail->AddAttachment("files/img03.jpg"; 
+            $mail->AddAddress($mail_destino, "Manuel Marin"); 
+            $mail->IsHTML(true); 
+            if(!$mail->Send()) 
+            { 
+
+                $output=array('error'=> '1','msg'=>( $mail->ErrorInfo));
+                echo json_encode($output,JSON_FORCE_OBJECT);
+                
+            }
+            else 
+            {
+                $output=array('error'=> '0','msg'=>'Código enviado el email');
+                echo json_encode($output,JSON_FORCE_OBJECT);
+                
+            }
+        }
+            /*else
+            {
+                echo true;
+            }*/
+        }
+	
+ 
+?>
