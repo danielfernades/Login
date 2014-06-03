04f5748c4a652d1467fc608ddd39779a161fb258
mmarin16 <m.marin16@hotmail.com>
2014-06-02 23:41:26 -0500

    mas
    mas

--- main.php
+++ main.php
@@ -28,39 +28,54 @@ if (isset($tag) && $tag !== '') {
 
         elseif ($tag=='forgot')
         {
-            include("class.phpmailer.php"); 
-            include("class.smtp.php"); 
             $mail_destino=$_POST['email'];
-            $mail = new PHPMailer(); 
-            $mail->IsSMTP(); 
-            $mail->SMTPAuth = true; 
-            $mail->SMTPSecure = "ssl"; 
-            $mail->Host = "smtp.gmail.com"; 
-            $mail->Port = 465; 
-            $mail->Username = "mm.marin16@gmail.com"; 
-            $mail->Password = "alejandro1990";
-            $mail->From = "mm.marin16@gmail.com"; 
-            $mail->FromName = "Test Login"; 
-            $mail->Subject = "Recupera tu contraseña!"; 
-            $mail->AltBody = "Este es un mensaje de prueba."; 
-            $mail->MsgHTML("<b>Este es un mensaje de prueba</b>."); 
-            //$mail->AddAttachment("files/files.zip"; 
-            //$mail->AddAttachment("files/img03.jpg"; 
-            $mail->AddAddress($mail_destino, "Manuel Marin"); 
-            $mail->IsHTML(true); 
-            if(!$mail->Send()) 
-            { 
-
-                $output=array('error'=> '1','msg'=>( $mail->ErrorInfo));
-                echo json_encode($output,JSON_FORCE_OBJECT);
-                
-            }
-            else 
+            $conexion = mysql_connect("localhost", "root", "");
+            mysql_select_db("prueba", $conexion);
+            $queEmp = "SELECT * FROM usuarios where email='".$mail_destino."'";
+            $resEmp = mysql_query($queEmp,$conexion)or die(mysql_error());
+            if  (mysql_num_rows($resEmp)>0)
             {
-                $output=array('error'=> '0','msg'=>'Código enviado el email');
-                echo json_encode($output,JSON_FORCE_OBJECT);
-                
+                include("class.phpmailer.php"); 
+                include("class.smtp.php"); 
+               
+                $mail = new PHPMailer(); 
+                $mail->IsSMTP(); 
+                $mail->SMTPAuth = true; 
+                $mail->SMTPSecure = "ssl"; 
+                $mail->Host = "smtp.gmail.com"; 
+                $mail->Port = 465; 
+                $mail->Username = "mm.marin16@gmail.com"; 
+                $mail->Password = "alejandro1990";
+                $mail->From = "mm.marin16@gmail.com"; 
+                $mail->FromName = "Test Login"; 
+                $mail->Subject = "Recupera tu contraseña!"; 
+                $mail->AltBody = "Este es un mensaje de prueba."; 
+                $mail->MsgHTML("<b>Este es un mensaje de prueba</b>."); 
+                //$mail->AddAttachment("files/files.zip"; 
+                //$mail->AddAttachment("files/img03.jpg"; 
+                $mail->AddAddress($mail_destino, "Manuel Marin"); 
+                $mail->IsHTML(true); 
+                if(!$mail->Send()) 
+                { 
+
+                    $output=array('error'=> '1','msg'=>( $mail->ErrorInfo));
+                    echo json_encode($output,JSON_FORCE_OBJECT);
+                    
+                }
+                else 
+                {
+                    $output=array('error'=> '0','msg'=>'Código enviado el email');
+                    echo json_encode($output,JSON_FORCE_OBJECT);
+                    
+                }
             }
+            else
+            {
+                  $output=array('error'=> '1','msg'=>'El email no esta registrado');
+                  echo json_encode($output,JSON_FORCE_OBJECT);
+            }
+
+           
         }
             /*else
             {
